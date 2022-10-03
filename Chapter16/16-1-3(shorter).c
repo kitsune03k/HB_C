9726fb853ca4900ebcf97794fc535903bbb9125a
kitsune03 <kitsune03@outlook.com>
2022-10-03 08:02:19 +0000

    dd

--- Chapter16/16-1-3(shorter).c
+++ Chapter16/16-1-3(shorter).c
@@ -12,36 +12,28 @@ int main(void)
 	for(i=0; i<4; i++){
 		printf("%s의\n", strNAME[i]);
 		for(j=0; j<4; j++){
+			int tmp = 0;
 			printf("%s점수 : ", strSUB[j]);
 			scanf("%d", &arr[i][j]);
-		}
-		printf("\n");
-	}
-	
-	for(i=0; i<4; i++){
-		int tmp = 0;
-		for(j=0; j<4; j++){
 			arr[i][4] += arr[i][j];
 		}
-	}//학생별 총점 계산
+		printf("\n");
+	}//입력, 학생별 총점 계산
+	
 	
 	for(j=0; j<4; j++){
 		int tmp = 0;
 		for(i=0; i<4; i++){
 			arr[4][j] += arr[i][j];
 		}
-	}//과목별 총점 계산
-	
-	for(i=0; i<4; i++){
-		int tmp = 0;
-		arr[4][4] += arr[i][4];
-	}//arr[4][4] 계산
+		arr[4][4] += arr[4][j];
+	}//과목별 총점 계산, arr[4][4] 계산
 	
 	for(i=0; i<5; i++){
 		for(j=0; j<5; j++){
 			printf("%-4d", arr[i][j]);
 		}
 		printf("\n");
-	}// 출력
+	}//출력
 	
 }
