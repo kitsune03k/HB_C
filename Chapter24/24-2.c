d2ae93ad687c74eef3d6690977bf2f826bca6691
kitsune03 <kitsune03@outlook.com>
2022-10-17 14:11:13 +0000

    dd

--- Chapter24/24-2.c
+++ Chapter24/24-2.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int calcfilesize(FILE* fp)
+long calcfilesize(FILE* fp)
 {
-	int fpos;
+	long fpos;
 	
 	while(1){
 		fpos = ftell(fp);
-		int buf = fgetc(fp);
+		char buf = fgetc(fp);
+		// 스트림 대상이 텍스트파일인 경우 텍스트 파일 내용 출력
 		
 		if(buf==EOF) 
 			break;
@@ -14,24 +15,30 @@ int calcfilesize(FILE* fp)
 		if문 조건에서 fgetc 쓰면 안됨!!
 		fputc fgetc 전부 파일포인터 한개씩 이동
 		EOF 확인하려 if문 조건에서 fget쓰면 while문 1사이클당 파일포인터 2개씩 이동
-		putchar 한칸씩 건너뛰어서 작동
+		-> putchar 한칸씩 건너뛰어서 작동
 		*/
+		
+		putchar(buf); // 스트림 대상이 텍스트가 아닐경우 주석처리!
 	}
-	printf("포인터 위치 : %d\n", fpos);
+	printf("\n포인터 위치 : %ld\n", fpos);
 	
-	return fpos;
+	long fpos_reset = fseek(fp, 0, 0);
+	
+	printf("파일포인터 초기화 후 위치 : %ld\n", fpos_reset);
+
+	return fpos;	
 }
 
 
 int main(void)
 {
-	FILE* fp = fopen("whatsmysize.png.bin", "rb");
+	FILE* fp = fopen("whatsmysize.txt", "rb");
 	if( fp == NULL ){
 		puts("파일 오픈 실패!");
 		return -1;
 	}
 	
-	printf("파일의 사이즈는 %dbyte 입니다\n",calcfilesize(fp));
+	printf("파일의 사이즈는 %ldbyte 입니다\n",calcfilesize(fp));
 
 	fclose(fp);
 	
